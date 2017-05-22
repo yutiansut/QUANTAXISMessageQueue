b0997429a5b0a47e536fa5d0db07c3d034a0f884
yutiansut <yutiansut@qq.com>
2017-05-22 18:08:56 +0800

    #commit python

---
+++ python/comsumer/get_message.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+from .setting import *
+import pykafka
+import kafka
+
+class message_client():
+    def _init_(self):
+        pass
---
+++ python/comsumer/setting.py
@@ -0,0 +1,2 @@
+#coding:utf-8
+producer_port =9092
